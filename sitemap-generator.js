ef37aaa1fd0f6ea211828f1879f504a67c937d4f
Doocey <doocey1@gmail.com>
2021-04-04 16:21:34 +0100

    SEO: Add a Sitemap generator to see if we can do some SEO goodness on build. Tidy up player name in profile. Also put 404 into its own folder.

---
+++ sitemap-generator.js
@@ -0,0 +1,19 @@
+const { configureSitemap } = require('@sergeymyssak/nextjs-sitemap');
+
+console.log(process)
+
+const Sitemap = configureSitemap({
+  baseUrl: 'https://fpldashboard.dev',
+  exclude: ['/api/*', '/404/', '/player/*'],
+  excludeIndex: true,
+  pagesConfig: {
+    '/price-changes/': {
+      priority: '0.9',
+      changefreq: 'daily',
+    },
+  },
+  isTrailingSlashRequired: true,
+  targetDirectory: __dirname + '/public',
+  pagesDirectory: __dirname + '/pages',
+});
+Sitemap.generateSitemap();
