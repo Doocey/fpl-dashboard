f066cf4493a466cae0930c15205ac36e6df77786
Doocey <doocey1@gmail.com>
2022-08-18 21:59:55 -0400

    Update for the 22/23 Premier League season

--- next.config.js
+++ next.config.js
@@ -6,4 +6,11 @@ module.exports = {
       'source.unsplash.com'
     ],
   },
+  webpack: (config) => {
+    // this will override the experiments
+    config.experiments = { ...config.experiments, ...{ topLevelAwait: true } };
+    // this will just update topLevelAwait property of config.experiments
+    // config.experiments.topLevelAwait = true 
+    return config;
+  },
 }
