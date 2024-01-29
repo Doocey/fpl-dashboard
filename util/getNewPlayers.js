be999710eacac6f091a5c25056af6151e0f74256
Kevin Doocey <104010062+kevdoocey@users.noreply.github.com>
2024-01-29 19:51:08 +0000

    Make sure newly added FPL players are being reflected in our Mongo collection

--- util/getNewPlayers.js
+++ util/getNewPlayers.js
@@ -1,37 +1,39 @@
-import { connectToDatabase } from './mongodb'
-import { getDatabasePrices } from './getDatabasePrices'
-import { getLivePlayerPrices } from './getLivePlayerPrices'
-const { MONGODB_PRICE_COLLECTION } = process.env
+import { connectToDatabase } from "./mongodb";
+import { getDatabasePrices } from "./getDatabasePrices";
+import { getLivePlayerPrices } from "./getLivePlayerPrices";
+const { MONGODB_PRICE_COLLECTION } = process.env;
+const { db } = await connectToDatabase();
 
 export async function getNewPlayers() {
-  // Grab live prices & stored prices to compare against
-  const { db } = await connectToDatabase();
-  const live_players = await getLivePlayerPrices()
-  const db_players = await getDatabasePrices()
-  let status;
-
   try {
-    // Compare both arrays of players, and see if there's differences.
-    // May or may not have been taking directly from Stackoverflow :P 
-    const new_player = live_players.filter(({ id: id1 }) => !db_players.some(({ _id: id2 }) => id2 === id1));
+    // Grab live prices & stored prices to compare against
+    const live_players = await getLivePlayerPrices();
+    const db_players = await getDatabasePrices();
 
-    if (!new_player.length === 0) {
-      for await (const player of new_player) {
-        await db.collection(MONGODB_PRICE_COLLECTION).insertOne({
-          _id: player.id,
-          first_name: player.first_name,
-          second_name: player.second_name,
-          web_name: player.web_name,
-          price: player.now_cost
-        })
-      }
-      status = `${new_player.length} new player added`;
+    const idsOfLivePlayers = live_players.map(
+      ({ id, first_name, second_name, now_cost, web_name }) => ({
+        _id: id,
+        first_name,
+        second_name,
+        web_name,
+        price: now_cost
+      })
+    );
+
+    const idsOfDbPlayers = db_players.map(({ _id }) => _id);
+
+    const newlyAddedPlayers = idsOfLivePlayers.filter(
+      (player) => !idsOfDbPlayers.includes(player._id)
+    );
+
+    if (newlyAddedPlayers.length) {
+      await db.collection(MONGODB_PRICE_COLLECTION).insertMany(newlyAddedPlayers);
+
+      return `${newlyAddedPlayers.length} newly added`;
     } else {
-      status = 'No new players to be added'
+      return "No new players to be added";
     }
-    return status
   } catch (error) {
-    console.log('Something went wrong')
-    return error
+    return "Something went wrong";
   }
-};
+}
