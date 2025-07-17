2e1ef02f932498bebaf8288128db38e4a8ad3edc
Doocey <doocey1@gmail.com>
2025-07-17 15:51:07 +0100

    Add Bluesky library, via ATProto

---
+++ pages/api/bluesky/index.js
@@ -0,0 +1,19 @@
+/**
+ * Endpoint to hit to post price changes to Bluesky, if there are any.
+ * Hit by cron job to automate it
+ */
+
+export default async (req, res) => {
+  // Peform some basic auth before executing
+  const secret = req.headers["x-webhook-secret"];
+
+  if (secret !== process.env.WEBHOOK_SECRET) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  try {
+    res.status(200).send("Hello, Bluesky");
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+};
