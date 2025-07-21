63bcbe2c5ffb59db24b47a57aea57e1d0ddbd381
Doocey <doocey1@gmail.com>
2025-07-21 15:17:15 +0100

    Upgrade Mongo driver ahead of future dep changes

--- util/mongodb.js
+++ util/mongodb.js
@@ -2,15 +2,9 @@ import { MongoClient } from "mongodb";
 
 const { MONGODB_URI, MONGODB_DB } = process.env;
 
-if (!MONGODB_URI) {
+if (!MONGODB_URI || !MONGODB_DB) {
   throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
-}
-
-if (!MONGODB_DB) {
-  throw new Error(
-    "Please define the MONGODB_DB environment variable inside .env.local"
+    "Please define the MONGODB_URI & MONGODB_DB environment variable inside .env.local"
   );
 }
 
@@ -19,24 +13,19 @@ if (!MONGODB_DB) {
  * in development. This prevents connections growing exponentially
  * during API Route usage.
  */
-let cached = global.mongo;
+let cached = globalThis.mongo;
 
 if (!cached) {
-  cached = global.mongo = { conn: null, promise: null };
+  cached = globalThis.mongo = { conn: null, promise: null };
 }
 
 export async function connectToDatabase() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+  if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    const opts = {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    };
+    const client = new MongoClient(MONGODB_URI);
 
-    cached.promise = MongoClient.connect(MONGODB_URI, opts).then((client) => {
+    cached.promise = client.connect().then(() => {
       return {
         client,
         db: client.db(MONGODB_DB)
